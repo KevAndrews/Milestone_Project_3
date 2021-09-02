5100fde08bd28056d72816c4abad54ef253a4d98
Kevin Andrews <nivek87@gmail.com>
2021-09-02 14:01:37 +0000

    Update app file to remove DB call

--- app.py
+++ app.py
@@ -11,13 +11,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+            
