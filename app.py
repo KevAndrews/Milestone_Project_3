de386163d723a4c342ccdb940bd99d17e4516914
Kevin Andrews <nivek87@gmail.com>
2021-09-05 13:42:18 +0000

    Add Sign up functionality and base profile html

--- app.py
+++ app.py
@@ -31,7 +31,43 @@ def login():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+        
+        # Custom code
+        if request.form.get("password") == request.form.get("conpassword"):
+            register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+            mongo.db.users.insert_one(register)
+
+            # put the new user into 'session' cookie
+            session["user"] = request.form.get("username").lower()
+            return redirect(url_for("profile", username=session["user"]))
+        else:
+            flash("Passwords do not match")
+            return redirect(url_for("signup"))
+
     return render_template("signup.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
